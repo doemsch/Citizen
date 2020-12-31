96049ce26a6cab53105946c8b56e1fe93df46b99
doemsch <76711054+doemsch@users.noreply.github.com>
2020-12-30 22:16:07 -0600

    Add files via upload

--- hundred_questions.py
+++ hundred_questions.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
-# Let's get started here!
+# let's get started here!
 
 # the overall goal is to create a quiz that asks me 20 randomly selected questions that are stored in a .csv file
 # the answers are multiple choice (4) and there shall be one correct answer and three incorrect answers
-# when I answer a question using the letters A-D, I shall get "Great job!" if correct or "More luck next time!" if incorrect
+# when i answer a question using the letters A-D, i shall get "Great job!" if correct or "More luck next time!" if incorrect
 # after the twenty questions the score shall be displayed as a percentage
-# I shall be able to exit out of the quiz by typing "X"
+# i shall be able to exit out of the quiz by typing "X"
 
-# I will outline my thoughts on how I envision this might happen in the sections below so that later I can work on them! :-)
-# I will do some research as I go along...
+# i will outline my thoughts on how I envision this might happen in the sections below so that later i can work on them! :-)
+# i will do some research as I go along...
 
 
-# First I need to be able to work with the data from the Excel spreadsheet
+# firstirst i need to be able to work with the data from the Excel spreadsheet
 
 import csv
 
@@ -21,9 +21,13 @@ with open("D:/Doemsch/Python/citizen_q_and_a.csv","r") as citizen:
     print(this_file)
 
 
-# Any idea why this does not print the data from the Excel spreadsheet?
+# any idea why this does not print the data from the Excel spreadsheet?
+# actually it does
 
 
+# okay, next step
+
+# this could be helpful: https://larsesdohr.com/create-a-multiple-choice-test-from-an-excel-sheet-with-python-and-openpyxl/
 
 
 
