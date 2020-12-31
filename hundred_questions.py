a19526d13902bbe7cf3bf678c0871e434c1352db
doemsch <76711054+doemsch@users.noreply.github.com>
2020-12-30 20:31:18 -0600

    Add files via upload

---
+++ hundred_questions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Let's get started here!
+
+# the overall goal is to create a quiz that asks me 20 randomly selected questions that are stored in a .csv file
+# the answers are multiple choice (4) and there shall be one correct answer and three incorrect answers
+# when I answer a question using the letters A-D, I shall get "Great job!" if correct or "More luck next time!" if incorrect
+# after the twenty questions the score shall be displayed as a percentage
+# I shall be able to exit out of the quiz by typing "X"
+
+
+
+
+
+
